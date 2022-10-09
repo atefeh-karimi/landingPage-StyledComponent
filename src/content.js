32ee95303da6e9ed5684abcc4df11fed497aa2f2
atefeh-karimi <atefehkarimikia0922@gmail.com>
2022-10-09 14:52:35 +0330

    fixed img url

--- src/content.js
+++ src/content.js
@@ -3,18 +3,18 @@ export const data = [
     id: 1,
     title: "Grow Together",
     body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat",
-    image: "/image/illustration-grow-together.svg",
+    image: "./image/illustration-grow-together.svg",
   },
   {
     id: 2,
     title: "Flowing Conversation",
     body: "McClintock's eye for detail certainly helped narrow the whereabouts of lorem ipsum's origin, however, the “how and when” still remain something of a mystery, with competing theories and timelines.",
-    image: "/image/illustration-flowing-conversation.svg",
+    image: "./image/illustration-flowing-conversation.svg",
   },
   {
     id: 3,
     title: "Your Users",
     body: "The passage experienced a surge in popularity during the 1960s when Letraset used it on their dry-transfer sheets, and again during the 90s as desktop publishers bundled the text with their software. ",
-    image: "/image/illustration-your-users.svg",
+    image: "./image/illustration-your-users.svg",
   },
 ];
